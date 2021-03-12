a16238e5d0b97b07dcb5b6da99dcd8504afca4cd
mattyoungberg <matt_youngberg@outlook.com>
2021-03-11 20:33:09 -0700

    Refactors to MergeSort for optimization.

--- src/main/java/com/mattyoungberg/MergeSort/MergeSort.java
+++ src/main/java/com/mattyoungberg/MergeSort/MergeSort.java
@@ -2,22 +2,27 @@ package com.mattyoungberg.MergeSort;
 
 public class MergeSort {
 
-    public static void mergeSort(int[] array, int arrayLength) {
+    public static void mergeSort(int[] array) {
+        int arrayLength = array.length;
         if (arrayLength < 2)
             return;
         int midPoint = arrayLength / 2;
+
         int[] leftArray = new int[midPoint];
         int[] rightArray = new int[arrayLength - midPoint];
         for(int i = 0; i < midPoint; i++)
             leftArray[i] = array[i];
         for(int j = midPoint; j < arrayLength; j++)
             rightArray[j - midPoint] = array[j];
-        mergeSort(leftArray, midPoint);
-        mergeSort(rightArray, arrayLength-midPoint);
-        merge(array, leftArray, rightArray, midPoint, arrayLength-midPoint);
+
+        mergeSort(leftArray);
+        mergeSort(rightArray);
+        merge(array, leftArray, rightArray);
     }
 
-    public static void merge(int[] array, int[] leftArray, int[] rightArray, int leftLength, int rightLength) {
+    public static void merge(int[] array, int[] leftArray, int[] rightArray) {
+        int leftLength = leftArray.length;
+        int rightLength = rightArray.length;
         int i = 0, j = 0, k = 0;
         while (i < leftLength && j < rightLength) {
             if (leftArray[i] < rightArray[j])
