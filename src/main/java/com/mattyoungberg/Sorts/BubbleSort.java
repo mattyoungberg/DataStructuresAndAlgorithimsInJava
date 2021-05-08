9d1ef3ff9a3385d9b64fe20b739579b8660a434c
mattyoungberg <matt_youngberg@outlook.com>
2021-05-08 14:27:56 -0600

    Initial commit of `BubbleSort.java`

---
+++ src/main/java/com/mattyoungberg/Sorts/BubbleSort.java
@@ -0,0 +1,18 @@
+package com.mattyoungberg.Sorts;
+
+public class BubbleSort {
+
+    public static void bubbleSort(int[] array) {
+        int outer, inner;
+        for (outer = array.length - 1; outer > 1; outer--)
+            for (inner = 0; inner < outer; inner++)
+                if (array[inner] > array[inner + 1])
+                    BubbleSort.swap(array, inner, inner + 1);
+    }
+
+    private static void swap(int[] array, int i, int j) {
+        int temp = array[i];
+        array[i] = array[j];
+        array[j] = temp;
+    }
+}
